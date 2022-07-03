fd28e5938fd10d8c5347aec615a5b98ba7c73d5c
JustinotherGitter <justin.jb78@gmail.com>
2022-07-03 20:53:27 +0200

    refined navigation menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,25 @@
 function toggleSideNav(item) {
-    item.classList.toggle("change");
-
-    changeNavWidth();
-}
-
-function changeNavWidth() {
-    isClosed = document.getElementById("SideNav").style.width == "52px";
+    let width = document.getElementById("SideNav").style.width;
+    let labels = Array.from(document.getElementsByClassName("nav-label"));
+    let isClosed = width == "" || width == "64px";
     
     if (isClosed == true) {
         document.getElementById("SideNav").style.width = "200px";
         document.getElementById("main").style.marginLeft = "200px";
+        document.getElementById("toggle-icon").textContent = "Close";
+
+        labels.forEach(element => {
+            console.log(element);
+            element.classList.remove("label-transparent");
+        });
+
     } else {
-        document.getElementById("SideNav").style.width = "52px";
-        document.getElementById("main").style.marginLeft = "52px";
+        document.getElementById("SideNav").style.width = "64px";
+        document.getElementById("main").style.marginLeft = "64px";
+        document.getElementById("toggle-icon").textContent = "Menu";
+        
+        labels.forEach(element => {
+            element.classList.add("label-transparent");
+        });
     }
 }
